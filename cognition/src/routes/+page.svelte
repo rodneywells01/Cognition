1acb7453859977c335ba1936a3d3b8f84032b942
Rodney Wells <rodneywells01@gmail.com>
2023-02-05 16:02:32 -0600

    Firebase initial setup

--- cognition/src/routes/+page.svelte
+++ cognition/src/routes/+page.svelte
@@ -1,13 +1,46 @@
 <script>
+
+    import { initializeApp } from "firebase/app";
+    import { getFirestore } from "firebase/firestore";
+    import { collection, addDoc } from "firebase/firestore";
+
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyAFni6MVlrQTtsbWK9aVjhks6uQShZWZ-E",
+        authDomain: "cognition-7eb3f.firebaseapp.com",
+        projectId: "cognition-7eb3f",
+        storageBucket: "cognition-7eb3f.appspot.com",
+        messagingSenderId: "700759219266",
+        appId: "1:700759219266:web:79e29277901ff02de861db",
+        measurementId: "G-9680PX1957"
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+
+
     let name = "Rodney Wells"
     function changeName() {
         name = "John Doe"
     }
 
-    function save_current_cognition() {
-        console.log("Saving cognition")
-        console.log("Title: " + title)
-        console.log("Body: " + body)
+    async function save_current_cognition() {
+        console.log(db.app.name);
+        try {
+            console.log("Trying to save");
+            const docRef = await addDoc(collection(db, "cognition"), {
+                user: "rodneywells01",
+                title: title,
+                body: body
+            });
+            console.log("Document written with ID: ", docRef.id);
+            console.log("Cognition Saved")
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+
+
+
     }
 
 
