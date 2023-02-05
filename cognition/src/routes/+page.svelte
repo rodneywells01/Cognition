dfafaa7b940a847da3dac3f0f4b6c5242f320d89
Rodney Wells <rodneywells01@gmail.com>
2023-02-05 15:18:54 -0600

    Bare minimum interface

--- cognition/src/routes/+page.svelte
+++ cognition/src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let name = "Rodney Wells"
+    function changeName() {
+        name = "John Doe"
+    }
+
+    function save_current_cognition() {
+        console.log("Saving cognition")
+        console.log("Title: " + title)
+        console.log("Body: " + body)
+    }
+
+
+    let title = "Untitled"
+    let body = ""
+
+</script>
+
+<h3>Hello {name}</h3>
+
+<button on:click|once={changeName}>Change Name</button>
+
+<h1>{title}</h1>
+<input bind:value={title}>
+
+<textarea bind:value={body}></textarea>
+
+<button on:click={save_current_cognition}>Save</button>
